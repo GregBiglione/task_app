74488433d03f5f8d5dcd0be077afa89801521822
GregBiglione <gregory.biglione81@gmail.com>
2022-05-25 17:11:57 +0200

    Added colors

---
+++ lib/constant/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color yellowLight = Color(0xFFFFF7EC);
+const Color yellow = Color(0xFFFAF0DA);
+const Color yellowDark = Color(0xFFEBBB7F);
+
+const Color redLight = Color(0xFFFCF0F0);
+const Color red = Color(0xFFFBE4E6);
+const Color redDark = Color(0xFFF08A8E);
+
+const Color blueLight = Color(0xFFEDF4FE);
+const Color blue = Color(0xFFE1EDFC);
+const Color blueDark = Color(0xFFC0D3F8);
