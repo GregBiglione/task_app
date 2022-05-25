ba0a22ae68ab5115134206a1417f2c19f62bd455
GregBiglione <gregory.biglione81@gmail.com>
2022-05-25 21:39:15 +0200

    Added TaskList

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task_app/screen/home_app_bar.dart';
 import 'package:task_app/widget/go_premium.dart';
+import 'package:task_app/widget/task_list.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,6 +18,19 @@ class HomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           GoPremium(),
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "Tasks",
+              style: TextStyle(
+                fontSize:22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+              child: TaskList()
+          ),
         ],
       ),
     );
