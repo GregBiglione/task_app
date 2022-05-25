a765fda3a55c6d05c5545a3af4bcb05218e198f4
GregBiglione <gregory.biglione81@gmail.com>
2022-05-25 21:56:50 +0200

    Added Task object

---
+++ lib/model/task.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Task {
+  IconData? iconData;
+  String? title;
+  Color? bgColor;
+  Color? iconColor;
+  Color? btnColor;
+  num? left;
+  num? done;
+  bool? isLast;
+
+  Task(this.iconData, this.title, this.bgColor, this.iconColor, this.btnColor,
+      this.left, this.done, this.isLast);
+}
