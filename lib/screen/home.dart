846cc3195e6db6745ea77e251c835882e2c412df
GregBiglione <gregory.biglione81@gmail.com>
2022-05-25 21:20:12 +0200

    Added GoPremium

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_app/screen/home_app_bar.dart';
+import 'package:task_app/widget/go_premium.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,9 +13,12 @@ class HomePage extends StatelessWidget {
         preferredSize: const Size.fromHeight(50),
         child: HomeAppBar(),
       ),
-      body: Center(
-        child: Text("Home Screen"),
-      )
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GoPremium(),
+        ],
+      ),
     );
   }
 }
